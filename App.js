7a8e5700df52225ecf38f883ff4dcc20651836e1
Anirban Dutta <anirbandutta428@gmail.com>
2022-11-02 19:49:00 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,40 @@
+import { useState } from  "react";
+import './App.css';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+
+const modules = {
+  toolbar: [
+    // options here
+    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+    ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+  [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']         
+]
+}
+
+const App = () => {
+  const [value, setValue] =  useState("");
+  
+  return (
+    <div>
+      <h1>Text Editor</h1>
+      <ReactQuill modules={modules} onChange={setValue} theme="snow" placeholder="Start writing here"/>
+    </div>
+  )
+}
+
+export default App;
